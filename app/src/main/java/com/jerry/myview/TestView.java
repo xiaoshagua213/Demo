1389f2b4c81ea2ed660dc165e606dc125f2984e9
jerry <1821984695@qq.com>
2018-10-23 10:36:13 +0800

    first commit

---
+++ app/src/main/java/com/jerry/myview/TestView.java
@@ -0,0 +1,22 @@
+package com.jerry.myview;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by jerry on 2018/9/10.
+ *TestView 自定义Viewemo
+ */
+
+public class TestView extends View {
+
+    public TestView(Context context) {
+        super(context);
+    }
+
+    public TestView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
